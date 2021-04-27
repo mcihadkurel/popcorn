0a661e2e7bae3973ce50c34b27adc3736a7a8ee8
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-04-27 14:41:32 +0000

    Update webpack and compile first component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+import Movie from "./components/Movie";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const movies = ["1", "2", "3"];
+
+  return <div style={{color: '#ccc'}}>
+    {movies.map(movie => (
+     <Movie/> 
+    ))}
+  </div>
 }
 
 export default MyApp;
---
+++ pages/components/Movie.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Movie = () => (
+  <div className="movie">
+    This is a movie Component
+  </div>
+);
+
+export default Movie;
