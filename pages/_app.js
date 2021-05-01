4717cc3f998a35ad0abc8d6e0fa7a627ecd5de56
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-05-01 07:31:22 +0000

    display image sand styling

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import Movie from "./components/Movie";
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 function MyApp({ Component, pageProps }) {
@@ -17,13 +16,13 @@ function MyApp({ Component, pageProps }) {
   const loadData = async () => {
     const response = await fetch(FEATURED_API);
     const movies = await response.json();
-    setMovies(movies);
+    setMovies(movies.results);
     console.log(movies);
   }
 
-  return <div style={{color: '#ccc'}}>
+  return <div className="movie-container" style={{color: '#ccc'}}>
     {movies.length > 0 && movies.map((movie) =>
-      <Movie data={movie} />
+      <Movie key={movie.id} {...movie} />
     )}
 
   </div>
--- pages/components/Movie.js
+++ pages/components/Movie.js
@@ -1,7 +1,13 @@
 
-const Movie = () => (
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, overview, vote_average, popularity, release_date, genre_ids }) => (
   <div className="movie">
-    This is a movie Component
+    <img src={IMG_API + poster_path} alt={title} />
+    <div className="movie-info">
+      <h3>{title}</h3>
+      <span>{vote_average}</span>
+    </div>
   </div>
 );
 
