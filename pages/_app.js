59e0b8fb2b40a9583ecbd188515479f69b351845
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-05-01 08:27:59 +0000

    Add searchbar and styling

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import "../styles/globals.css";
 import { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 function MyApp({ Component, pageProps }) {
   const [movies, setMovies] = useState([]);
@@ -18,14 +20,19 @@ function MyApp({ Component, pageProps }) {
     const movies = await response.json();
     setMovies(movies.results);
     console.log(movies);
-  }
+  };
 
-  return <div className="movie-container" style={{color: '#ccc'}}>
-    {movies.length > 0 && movies.map((movie) =>
-      <Movie key={movie.id} {...movie} />
-    )}
-
-  </div>
+  return (
+    <>
+      <header>
+        <input type="text" placeholder="Search" className="text" />
+      </header>
+      <div className="movie-container" style={{ color: "#ccc" }}>
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
+    </>
+  );
 }
 
 export default MyApp;
