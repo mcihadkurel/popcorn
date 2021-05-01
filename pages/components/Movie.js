1441dab6713e9070424f9000988a722d19de42ae
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-05-01 07:45:21 +0000

    Add overview

--- pages/components/Movie.js
+++ pages/components/Movie.js
@@ -8,6 +8,11 @@ const Movie = ({ title, poster_path, overview, vote_average, popularity, release
       <h3>{title}</h3>
       <span>{vote_average}</span>
     </div>
+
+    <div className="movie-over">
+      <h2>Overview:</h2>
+      <p>{overview}</p>
+    </div>
   </div>
 );
 
